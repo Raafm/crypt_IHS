7f118733cfc6b5f19d5e7cf5c0ad78ae4c8dc739
rod <raafm@cin.ufpe.br>
2022-05-11 23:22:25 -0300

    controlar display

--- funcoes_aux.py
+++ funcoes_aux.py
@@ -55,5 +55,31 @@ def controlar_display(fd):
     red = os.write(fd, data2.to_bytes(4, 'little'))
     print("Display_R %d bytes"%red)
 
+def conversao_display(num):
+    if num == 0 or num == "0" : return 0x40
+    if num == 1 or num == "1" : return 0x79
+    if num == 2 or num == "2" : return 0x24
+    if num == 3 or num == "3" : return 0x30
+    if num == 4 or num == "4" : return 0x19
+    if num == 5 or num == "5" : return 0x12
+    if num == 6 or num == "6" : return 0x02
+    if num == 7 or num == "7" : return 0x78
+    if num == 8 or num == "8" : return 0x00
+    if num == 9 or num == "9" : return 0x10
+    print(f"erro de conversao: {num} is not a digit")
+
+def print_display(fd,data1 = 0,data2 = 0):
+
+    data1 = controlar_display(data1)
+    data2 = controlar_display(data2)
+    
+    ioctl(fd, WR_R_DISPLAY)
+    red = os.write(fd, data1.to_bytes(4, 'little'))
+    print("Display_L %d bytes"%red)
+    ioctl(fd, WR_L_DISPLAY)
+    red = os.write(fd, data2.to_bytes(4, 'little'))
+    print("Display_R %d bytes"%red)
+
+
 if __name__== "__main__" :
     main()
